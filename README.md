d8a2f533eb26320d8b85cc455d4520e688dc64cd
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-11 21:24:25 +0700

    feat: init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wiz
+
+A simple, frameless, lightweight browser for screenshots
+
+## Why Wiz?
+
+Screenshoting a website for demo perpose has been developer's common task and it's sometime quite annoying to cut the image screenshoted from the frame of a browser and that's why I created this simple browser.
+
+Wiz is built using [`tauri`](https://tauri.studio) framework and it's a cross-platform application. Also, unlike apps built on the top of Electron, it is lightweight! The installer is only 4MB. In addition, it's completely free.
+
+## How to use?
+You can start Wiz by running it via terminal. CLI Usage:
+
+```bash
+wiz [url to open]
+```
+example:
+```bash
+wiz https://google.com
+```
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,51 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use clap::{Arg, App};
+use tauri::Manager;
+fn main() {
+  
+  tauri::Builder::default()
+  .setup(|app| {
+    let matches = App::new("Wiz")
+      .version("0.1.0")
+      .author("Justin Maximillian Kimlim <kimlimjustin@gmail.com>")
+      .about("Open a url on Wiz")
+      .arg(Arg::new("url")
+          .about("Url to be opened on Wiz. http:// protocol can be ommited.")
+          .index(1))
+      .get_matches();
+    let mut url:String = "https://github.com/kimlimjustin/wiz".to_string();
+    if let Some(i) = matches.value_of("url") {
+      url = if i.starts_with("http://") || i.starts_with("https://") {
+        i.to_string()
+      } else {
+        format!("http://{}", i)
+      }
+    }
+    let main_window = app.get_window("main").unwrap();
+      
+    main_window.eval(format!("window.location.replace('{url}')", url=url).as_str()).unwrap();
+    Ok(())
+  })
+  .on_page_load(|window, _| {
+    window.eval("(() => {
+      const el = document.createElement('div')
+      el.setAttribute('data-tauri-drag-region','')
+      el.id = 'wiz-drag-area'
+      const style = document.createElement('style')
+      style.innerHTML='#wiz-drag-area{position:fixed;left:10px;top:10px;width:40px;height:40px;border-radius:50%;cursor:grab;-webkit-app-region:drag;z-index:99999999;}#wiz-drag-area:hover{background:#8885;}'
+      document.body.appendChild(el)
+      document.body.appendChild(style)
+      
+      const rootStyle = document.createElement('style')
+      rootStyle.innerHTML='::-webkit-scrollbar {display: none;}'
+      document.head.appendChild(rootStyle)
+      
+      })()").unwrap();
+  })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
